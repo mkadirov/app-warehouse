b986a2ffc97858e579cea52517651ec55ec29440
Kadirov1993 <mkodirjonov@mail.ru>
2023-03-01 22:37:53 +0100

    user

--- src/main/java/pdp/appwarehouse/controller/UserController.java
+++ src/main/java/pdp/appwarehouse/controller/UserController.java
@@ -1,8 +1,9 @@
 package pdp.appwarehouse.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pdp.appwarehouse.payload.Result;
+import pdp.appwarehouse.payload.UserDto;
 import pdp.appwarehouse.service.UserService;
 
 @RestController
@@ -11,4 +12,29 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @GetMapping
+    public Result getUserList(){
+        return userService.getUsersList();
+    }
+
+    @GetMapping("/{id}")
+    public Result getUserByID(@PathVariable Integer id){
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public Result addUser(@RequestBody UserDto userDto){
+        return userService.addUser(userDto);
+    }
+
+    @PutMapping("/{id}")
+    public Result uploadUser(@PathVariable Integer id, @RequestBody UserDto userDto){
+        return userService.uploadUserById(id, userDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public Result deleteUser(@PathVariable Integer id){
+        return userService.deleteUserById(id);
+    }
 }
